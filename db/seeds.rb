41aafa025c39a2e22d21125e3f9e5cb5482b3beb
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-05-14 02:00:46 -0500

    initial api

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+todos = Todo.create([{
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+},
+{
+  "id": 2,
+  "title": "quis ut nam facilis et officia qui",
+  "completed": false
+},
+{
+  "id": 3,
+  "title": "fugiat veniam minus",
+  "completed": false
+},
+{
+  "id": 4,
+  "title": "et porro tempora",
+  "completed": true
+}])
+ 
+puts('Seed')
